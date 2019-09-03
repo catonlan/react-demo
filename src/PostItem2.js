3aa0a94ceb1600d68a40902ace45cdf9c1f35285
catonlan <catonlan@163.com>
2019-09-03 23:35:31 +0800

    修改方法

--- src/PostItem2.js
+++ src/PostItem2.js
@@ -1,80 +1,51 @@
 import React,{Component} from 'react';
 
-// class PostItem extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             vote: 0
-//         };
-//     }
-//     // 处理点赞逻辑
+class PostItem2 extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            vote: 0
+        };
+    }
+    // 处理点赞逻辑
 
-//     handleClick() {
-//         let vote = this.state.vote;
-//         vote++;
-//         this.setState({
-//             vote: vote
-//         });
-//     }
+    handleClick() {
+        let vote = this.state.vote;
+        vote++;
+        this.setState({
+            vote: vote
+        });
+    }
 
-//     render() {
-//         const {title, author, date} = this.props;
-//         return (
-//             <li>
-//                 <div>
-//                     {title}
-//                 </div>
-//                 <div>
-//                     创建人：<span>{author}</span>
-//                 </div>
-//                 <div>
-//                     创建时间：<span>{date}</span>
-//                 </div>
-//                 <div>
-//                     <button
-//                         onClick={() => {
-//                             this.handleClick();
-//                         }}
-//                     >
-//                         点赞
-//                     </button>
-//                     &nbsp;
-//                     <span>
-//                         {this.state.vote}
-//                     </span>
-//                 </div>
-//             </li>
-//         );
-//     }
-// }
-function PostItem2(props) {
-    const handleClick = () => {
-        if(typeof props.onVote !=="undefined"){
-            props.onVote(props.post.id);
-        }
-    };
-
-    return (
-        <li>
-            <div>
-                {props.title}
-            </div>
-
-             <div>
-                 创建人：<span> {props.author} </span>
-            </div>
-
-            <div>
-                创建时间：<span> {props.date} </span>
-            </div>
-
-            <div>
-                <button onClick={handleClick}>点赞</button>
-                &nbsp;
-                <span> {props.vote} </span>
-            </div>
-        </li>
-    );
+    render() {
+        const {title, author, date} = this.props;
+        return (
+            <li>
+                <div>
+                    {title}
+                </div>
+                <div>
+                    创建人：<span>{author}</span>
+                </div>
+                <div>
+                    创建时间：<span>{date}</span>
+                </div>
+                <div>
+                    <button
+                        onClick={() => {
+                            this.handleClick();
+                        }}
+                    >
+                        点赞
+                    </button>
+                    &nbsp;
+                    <span>
+                        {this.state.vote}
+                    </span>
+                </div>
+            </li>
+        );
+    }
 }
 
 export default PostItem2;
--- src/PostItem3.js
+++ src/PostItem3.js
@@ -1,52 +1,5 @@
 import React,{Component} from 'react';
 
-// class PostItem extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             vote: 0
-//         };
-//     }
-//     // 处理点赞逻辑
-
-//     handleClick() {
-//         let vote = this.state.vote;
-//         vote++;
-//         this.setState({
-//             vote: vote
-//         });
-//     }
-
-//     render() {
-//         const {title, author, date} = this.props;
-//         return (
-//             <li>
-//                 <div>
-//                     {title}
-//                 </div>
-//                 <div>
-//                     创建人：<span>{author}</span>
-//                 </div>
-//                 <div>
-//                     创建时间：<span>{date}</span>
-//                 </div>
-//                 <div>
-//                     <button
-//                         onClick={() => {
-//                             this.handleClick();
-//                         }}
-//                     >
-//                         点赞
-//                     </button>
-//                     &nbsp;
-//                     <span>
-//                         {this.state.vote}
-//                     </span>
-//                 </div>
-//             </li>
-//         );
-//     }
-// }
 function PostItem3(props) {
     const handleClick = () => {
         if(typeof props.onVote !=="undefined"){
